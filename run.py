244ec6b31cca67eab1405cdf13e487d97d40afa8
TulaUnogi <>
2023-07-08 20:35:00 +0000

    Edit storytelling.py to only store variables, move functions to run.py

--- run.py
+++ run.py
@@ -2,10 +2,16 @@
 
 from pictures import *
 from storytelling import *
-from cprint import *
+import colorama
+from colorama import Fore, Style
 import sys
 import os
 from time import sleep
+
+
+# Initialize colorama
+colorama.init(autoreset=True)
+
 
 # Misc functions
 
@@ -17,13 +23,45 @@ def clear_terminal():
     os.system("clear")
 
 
+def small_text_bits(list):
+    '''
+    Displays small blocks of text one at the time
+    '''
+    for text in list:
+        for char in text:
+            sleep(0.07)
+            sys.stdout.write(char)
+            sys.stdout.flush()
+        sleep(1)
+        clear_terminal()
+    return text
+
+# Storytelling functions
+
+
+def intro():
+    '''
+    Prints the story intro with the typing and text delay effects
+    '''
+    os.system("clear")
+    name = input("Please enter your name: \n")
+    loading_bar = ["********************************************",
+                   "Your game starts now!"]
+    print(f"{Fore.YELLOW}Loading The Hangman...")
+    sleep(1)
+    print(small_text_bits(loading_bar), )
+    sleep(1.3)
+    os.system("clear")
+    small_text_bits(story_intro)
+
+
 def main_menu():
     """
     Displays the Main Menu to the user
     Takes user's input to call the menu functions
     """
-    cprint.fatal(welcome)
-    cprint.err(game_logo)
+    print(f"{Fore.YELLOW}{welcome}")
+    print(f"{Fore.RED}{game_logo}")
 
     left_option = "1. LET'S PLAY"
     middle_option = "2. HIGH SCORES"
@@ -32,36 +70,31 @@ def main_menu():
     menu_choice = input("Select an option: \n")
 
     if menu_choice == "1":
-        clear_terminal()
-        name = input("Please enter your name: \n")
-        loading_bar = ["......................", "Your game starts now!"]
-        cprint.warn("Loading The Hangman...")
-        sleep(1)
-        small_text_bits(loading_bar)
-        sleep(1.3)
         intro()
     elif menu_choice == "2":
         clear_terminal()
         print("The High Scores to be here")
     elif menu_choice == "3":
         clear_terminal()
-        cprint.warn("Are you leaving already?")
+        print(f"{Fore.YELLOW}{Style.BRIGHT}Are you leaving already?")
         exit_choice = input("Y = YES       N = NO  \n")
         if exit_choice.lower() == "y":
             clear_terminal()
             sleep(1)
-            exiting = ["""Exiting in progress...
-.........................""", "BYE- BYE ❤ "]
-            small_text_bits(exiting)
+            exiting = "Exiting in progress"
+            exiting_loading = ["""
+************************""", "❤ BYE-BYE ❤ "]
+            print(f"{Style.BRIGHT}{exiting}")
+            small_text_bits(exiting_loading)
             exit()
         elif exit_choice.lower() == "y":
             main_menu()
         else:
-            cprint.fatal("Please enter a valid option.")
+            print(f"{Fore.RED}Please enter a valid option.")
             sleep(1.5)
             main_menu()
     else:
-        cprint.fatal("Please enter a valid option.")
+        print(f"{Fore.RED}Please enter a valid option.")
         sleep(1.5)
         main_menu()
 
@@ -69,4 +102,3 @@ def main_menu():
 # Calling the storytelling functions
 
 main_menu()
-# meet_the_murderer()
--- storytelling.py
+++ storytelling.py
@@ -1,146 +1,108 @@
-import sys
-import os
-from time import sleep
-
-
-# Functions for managing of the text output used for storytelling
-
-def small_text_bits(list):
-    '''
-    Displays small blocks of text one at the time
-    '''
-    for text in list:
-        for char in text:
-            sleep(0.07)
-            sys.stdout.write(char)
-            sys.stdout.flush()
-        sleep(1)
-        os.system("clear")
-    return text
-
-
-# Functions for printing the string variables used for storytelling
-
-def intro():
-    '''
-    Prints the story intro with the typing and text delay effects
-    '''
-
-    story_intro = [
-        """
-        Uh, it's very dark tonight in the forest...
-        """, """
-        Hold on...
-        """, """
-        Where actually am I?
-        """, """
-        *PHONE CHECK*
-        """, """
-        There's no network coverage here...
-        """, """
-        Perfect.
-        """, """
-        Just PERFECT.
-        """, """
-        Ehhh, I should've downloaded the maps offline beforehand.
-        How did I even get in here?
-        """, """
-        Ah... Right...
-        There was a camping party with guys...
-        """, """
-        ...With a... bonfire...?
-        Somehow I'm struggling to remember.
-        """, """
-        Weird.
-        """, """
-        Anyway...
-        """, """
-        I most likely went for a little tinkle and got lost in the woods.
-        Now I'm cold and surrounded by trees. Great.
-        """, """
-        Let's find my way back.
-        I better leave an [X] mark on the ground.
-        It would be better to know, that I was already here...
-        """, """
-        Just in case.
-        """, """
-        Now, which direction should I go?"
-        """
-    ]
-    small_text_bits(story_intro)
-    return story_intro
-
-
-def meet_the_murderer():
+# String variables used for storytelling
+story_intro = [
     """
-    Prints the part of the story, when the User meets the murderer
+    Uh, it's very dark tonight in the forest...
+    """, """
+    Hold on...
+    """, """
+    Where actually am I?
+    """, """
+    *PHONE CHECK*
+    """, """
+    There's no network coverage here...
+    """, """
+    Perfect.
+    """, """
+    Just PERFECT.
+    """, """
+    Ehhh, I should've downloaded the maps offline beforehand.
+    How did I even get in here?
+    """, """
+    Ah... Right...
+    There was a camping party with guys...
+    """, """
+    ...With a... bonfire...?
+    Somehow I'm struggling to remember.
+    """, """
+    Weird.
+    """, """
+    Anyway...
+    """, """
+    I most likely went for a little tinkle and got lost in the woods.
+    Now I'm cold and surrounded by trees. Great.
+    """, """
+    Let's find my way back.
+    I better leave an [X] mark on the ground.
+    It would be better to know, that I was already here...
+    """, """
+    Just in case.
+    """, """
+    Now, which direction should I go?"
     """
-    go_ahead = [
-        """
-        Well, I guess I will just go ahead.
-        """, """
-        Everything is fine.
-        Just breath.
-        """, """
-        Huh... Huh...
-        All of that is quite spooky.
-        """, """
-        Wait.
-        I can see something between the trees!
-        """, """
-        It looks like a bonfire!
-        Am I finally getting close to our campsite?
-        I can't wait to finally have a company!
-        """, """
-        *Shadow in the dark*
-        'KHEE, KHEE, KHEE!
-        DID SOMEONE SAY THEY WISH TO HAVE A COMPANY? YEESSSS?'
-        """, """
-        LET ME BE YOUR COMPANY, MY FRIEND...
-        THE FORESTS THESE DAYS ARE NOT A SAFE PLACE TO BE,
-        NO-NO, THEY ARE NOT...
-        """, """
-        IT'S EASY TO GET LOST, IT'S EASY TO FALL...
-        ...INTO...
-        ...A TRAP!
-        BOO!
-        KHEE, KHEE, KHEE!
-        """, """
-        DID YOU ENJOY FINDING YOUR WAY TO THE BONFIRE? YEEESSS?
-        NO, SHUSHHH, DON'T ANSWER.
-        NO NEED, KHEE, KHEE...
-        """, """
-        YOUR FEAR AND CONFUSION ARE GOING TO END UP SOON, YEESSS.
-        I AM GOING TO END THEM UP WITH THIS ROPE AND...
-        OH, THAT TREE BRANCH OVER THERE, KHEE, KHEE!
-        """, """
-        HMMMM... UNLESS...?
-        YOU MAY ENTERTAIN ME FOR A LITTLE BIT MORE? YEESSS?
-        FOR- YOU SEE- I AM QUITE LONELY HERE.
-        ALL MY COMPANIONS ARE HANGING UP WAY TOO HIGH,
-        ANYWAY, THEY ARE NOT TOO CHATTY ANYMORE EITHER, KHEE, KHEE!
-        HOW FUN AND IRONIC WOULD IT BE TO PLAY A HANGMAN GAME!
-        """, """
-        I SEE YOUR FACE, YOU ARE NOT TOO ENTHUSIASTIC, I SEE, YEESSS...
-        MAYBE THEN LET ME MAKE THINGS MORE INTERESTING FOR YOU, YEESS?
-        """, """
-        IF YOU PLAY A GAME AND YOU GUESS THE WORD IN TIME- YOU LIVE.
-        YOU ARE FREE TO GO.
-        MY LOSS, KHEE, KHEE.
-        """, """
-        IF YOU REFUSE TO PLAY- YOU DIE. BYE- BYE.
-        """, """
-        IF YOU PLAY AND YOU LOSE... THEN... HOW DO THEY SAY IT...?
-        YOU LIVE ONLY ONCE! KHEE, KHEE, KHEE!
-        """, """
-        YOU GET IT? YOU DIE ONLY ONCE TOO!
-        YEESS!
-        AND THEN... HAPPY DAYS FOR ME! KHEE, KHEE!
-        """, """
-        SOOO? WHAT DO YOU THINK?
-        WILL YOU ENTERTAIN ME?
-        WILL YOU PLAY THE GAME?
-        """
-    ]
-    small_text_bits(go_ahead)
-    return meet_the_murderer()
+]
+
+go_ahead = ["""
+Well, I guess I will just go ahead.
+""", """
+Everything is fine.
+Just breath.
+""", """
+Huh... Huh...
+All of that is quite spooky.
+Wait.
+I can see something between the trees!
+""", """
+It looks like a bonfire!
+Am I finally getting close to our campsite?
+I can't wait to finally have a company!
+"""]
+murderer_intro = ["""
+*Shadow in the dark*
+KHEE, KHEE, KHEE!
+DID SOMEONE SAY THEY WISH TO HAVE A COMPANY? YEESSSS?
+""", """
+LET ME BE YOUR COMPANY, MY FRIEND...
+THE FORESTS THESE DAYS ARE NOT A SAFE PLACE TO BE,
+NO-NO, THEY ARE NOT...
+""", """
+IT'S EASY TO GET LOST, IT'S EASY TO FALL...
+...INTO...
+...A TRAP!
+BOO!
+KHEE, KHEE, KHEE!
+""", """
+DID YOU ENJOY FINDING YOUR WAY TO THE BONFIRE? YEEESSS?
+NO, SHUSHHH, DON'T ANSWER.
+NO NEED, KHEE, KHEE...
+""", """
+YOUR FEAR AND CONFUSION ARE GOING TO END UP SOON, YEESSS.
+I AM GOING TO END THEM UP WITH THIS ROPE AND...
+OH, THAT TREE BRANCH OVER THERE, KHEE, KHEE!
+""", """
+HMMMM... UNLESS...?
+YOU MAY ENTERTAIN ME FOR A LITTLE BIT MORE? YEESSS?
+FOR- YOU SEE- I AM QUITE LONELY HERE.
+ALL MY COMPANIONS ARE HANGING UP WAY TOO HIGH,
+ANYWAY, THEY ARE NOT TOO CHATTY ANYMORE EITHER, KHEE, KHEE!
+HOW FUN AND IRONIC WOULD IT BE TO PLAY A HANGMAN GAME!
+""", """
+I SEE YOUR FACE, YOU ARE NOT TOO ENTHUSIASTIC, I SEE, YEESSS...
+MAYBE THEN LET ME MAKE THINGS MORE INTERESTING FOR YOU, YEESS?
+""", """
+IF YOU PLAY A GAME AND YOU GUESS THE WORD IN TIME- YOU LIVE.
+YOU ARE FREE TO GO.
+MY LOSS, KHEE, KHEE.
+""", """
+IF YOU REFUSE TO PLAY- YOU DIE. BYE- BYE.
+""", """
+IF YOU PLAY AND YOU LOSE... THEN... HOW DO THEY SAY IT...?
+YOU LIVE ONLY ONCE! KHEE, KHEE, KHEE!
+""", """
+YOU GET IT? YOU DIE ONLY ONCE TOO!
+YEESS!
+AND THEN... HAPPY DAYS FOR ME! KHEE, KHEE!
+""", """
+SOOO? WHAT DO YOU THINK?
+WILL YOU ENTERTAIN ME?
+WILL YOU PLAY THE GAME?
+"""]
